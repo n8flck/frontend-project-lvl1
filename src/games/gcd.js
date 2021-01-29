5ea0d514749165cc53a907decb5b9034f6855e8b
Yury Bialou <nt.flick@gmail.com>
2021-01-29 17:27:16 +0300

    fix: fixed zero case gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,6 +5,9 @@ import {
 export const getGreatestCommonDivisor = (numbers) => {
   let absoluteNumber1 = Math.abs((Number.parseInt(car(numbers), 10)));
   let absoluteNumber2 = Math.abs((Number.parseInt(cdr(numbers), 10)));
+  if (absoluteNumber1 === 0) {
+    return absoluteNumber2;
+  }
   while (absoluteNumber1 !== absoluteNumber2) {
     if (absoluteNumber1 > absoluteNumber2) {
       absoluteNumber1 -= absoluteNumber2;
